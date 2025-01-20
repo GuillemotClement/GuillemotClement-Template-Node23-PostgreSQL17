33d5fa52911bf2aac455c800ff118b94e361e3a4
GuillemotClement <guillemot.clement@protonmail.com>
2025-01-20 23:06:27 +0100

    fix: add import for Type

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
 });
 
 app.listen(3010, () => {
